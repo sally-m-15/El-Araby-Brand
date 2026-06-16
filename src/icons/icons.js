d05bd180956efc2d2a21727e505636682e82fc55
sally mohammed <sallymohammed1521999@gmail.com>
2026-06-16 10:48:34 +0300

    assets: add project icons

---
+++ src/icons/icons.js
@@ -0,0 +1,32 @@
+import {
+  FaRegularEye,
+  FaRegularEnvelope,
+  MdLock,
+  RiSearchLine,
+  FaRegularBell,
+  FaRegularHeart,
+  FaCaretDown,
+  FaShoppingCart,
+  FaBars,
+  CoEg,
+  FaArrowCircleLeft,
+  BiTag,
+  
+} from "oh-vue-icons/icons";
+
+
+export const icons = [
+  FaRegularEye,
+  FaRegularEnvelope,
+  MdLock,
+  RiSearchLine,
+  FaRegularBell,
+  FaRegularHeart,
+  FaCaretDown,
+  FaShoppingCart,
+  FaBars,
+  CoEg,
+  FaArrowCircleLeft,
+      BiTag
+
+];
